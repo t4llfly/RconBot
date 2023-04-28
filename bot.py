80682db828aae80f40ad0d0bc9ffd5b08ba1bc33
TallFly <tallfly@vk.com>
2023-04-28 20:50:52 +0600

    добавил ещё пару команд

--- bot.py
+++ bot.py
@@ -1,24 +1,61 @@
 import discord
 from discord.ext import commands
-from mcstatus import JavaServer
+from discord import app_commands
 import mcrcon
 
-SERVER_ADDRESS = input("Введите IP сервера: ")
-SERVER_PORT = input("Введите порт сервера: ")
+RCON_IP = input("Введите IP rcon сервера: ")
+RCON_PASSWORD = input("Введите пароль rcon сервера: ")
 
-server = JavaServer(SERVER_ADDRESS, SERVER_PORT)
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
 
 @bot.event
 async def on_ready():
     print(f'Запущен как {bot.user}')
     await bot.tree.sync()
-@bot.tree.command(name="addtowl")
-async def addtowl(interaction: discord.Interaction, username: str):
 
-    with mcrcon.MCRcon('localhost', 'PASSWORD_FROM_SERVERPROPERTIES') as rcon:
+
+@bot.tree.command(name="whitelistadd", description="Добавить игрока в белый список")
+@app_commands.describe(username="Никнейм игрока на сервере Minecraft")
+async def whitelistadd(interaction: discord.Interaction, username: str):
+    global RCON_IP
+    global RCON_PASSWORD
+
+    with mcrcon.MCRcon(RCON_IP, RCON_PASSWORD) as rcon:
         rcon.command('whitelist add ' + username)
-
     await interaction.response.send_message(f'**{username}** был добавлен в белый список.')
+
+
+@bot.tree.command(name="whitelistremove", description="Убрать игрока из белого списка")
+@app_commands.describe(username="Никнейм игрока на сервере Minecraft")
+async def whitelistremove(interaction: discord.Interaction, username: str):
+    global RCON_IP
+    global RCON_PASSWORD
+
+    with mcrcon.MCRcon(RCON_IP, RCON_PASSWORD) as rcon:
+        rcon.command('whitelist add ' + username)
+    await interaction.response.send_message(f'**{username}** был убран из белого списка.')
+
+
+@bot.tree.command(name="banplayer", description="Забанить игрока на сервере")
+@app_commands.describe(username="Никнейм игрока на сервере Minecraft", time="Время бана", reason="Причина бана")
+async def banplayer(interaction: discord.Interaction, username: str, time: str, reason: str):
+    global RCON_IP
+    global RCON_PASSWORD
+
+    with mcrcon.MCRcon(RCON_IP, RCON_PASSWORD) as rcon:
+        rcon.command('ban ' + username + time + reason)
+    await interaction.response.send_message(f'**{username}** был забанен на **{time}**. Причина: **{reason}**')
+
+
+@bot.tree.command(name="unbanplayer", description="Разбанить игрока на сервере")
+@app_commands.describe(username="Никнейм игрока на сервере Minecraft")
+async def unbanplayer(interaction: discord.Interaction, username: str):
+    global RCON_IP
+    global RCON_PASSWORD
+
+    with mcrcon.MCRcon(RCON_IP, RCON_PASSWORD) as rcon:
+        rcon.command('pardon ' + username)
+    await interaction.response.send_message(f'**{username}** был разбанен на сервере.')
 
 bot.run('TOKEN')
