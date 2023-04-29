66432dad1c5f09acee8ce4e30a5c4b131e5aab91
TallFly <tallfly@vk.com>
2023-04-29 17:16:21 +0600

    попытка прописать права командам

--- bot.py
+++ bot.py
@@ -2,12 +2,16 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import mcrcon
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 RCON_IP = input("Введите IP rcon сервера: ")
 RCON_PASSWORD = input("Введите пароль rcon сервера: ")
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-
 
 @bot.event
 async def on_ready():
@@ -17,6 +21,7 @@ async def on_ready():
 
 @bot.tree.command(name="whitelistadd", description="Добавить игрока в белый список")
 @app_commands.describe(username="Никнейм игрока на сервере Minecraft")
+# не работает по какой-то причине @commands.has_role("757460298889429023")
 async def whitelistadd(interaction: discord.Interaction, username: str):
     global RCON_IP
     global RCON_PASSWORD
@@ -58,4 +63,4 @@ async def unbanplayer(interaction: discord.Interaction, username: str):
         rcon.command('pardon ' + username)
     await interaction.response.send_message(f'**{username}** был разбанен на сервере.')
 
-bot.run('TOKEN')
+bot.run(TOKEN)
